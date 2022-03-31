36191250b4c21b9e2c946153447b68084dda1d1d
Vinícius Pereira de Oliveira <vipoliveira2012@live.com>
2022-03-31 16:08:16 -0300

    Finished

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:alcool_ou_gasolina/ui/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    )
+  );
+}
